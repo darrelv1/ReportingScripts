28cda1c37a4db73c697e98c79bc41a85114c0e06
Darrel Valdiviezo <Darrel.Valdiviezo@reveraliving.com>
2022-11-10 12:44:13 -0500

    Staging all files in dic

--- execute.py
+++ execute.py
@@ -3,12 +3,13 @@ from BaseReport import *
 
 tfile = "Report-10-11-22.xlsx"
 
-March14_flow = flowthrough(tfile)
-March14_flow.printer()
+#March14_flow = flowthrough(tfile)
+#March14_flow.printer()
 
 
 March14_jobcap = Capital_Jobcostreport(tfile)
 March14_jobcap.printer()
+March14_jobcap.anaylsisReport()
 
 
 
